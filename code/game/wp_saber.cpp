6dba0d4e69a91ada0ae0e79a6cdfb4f6a7f00fac
Knights of the Force 2.1 <linken233@hotmail.com>
2017-09-22 14:59:35 -0400

    Revert "Another fix"
    This reverts commit 7ceb04dd6a5f32e63d894b560826ca4412281a4c.

--- code/game/wp_saber.cpp
+++ code/game/wp_saber.cpp
@@ -881,7 +881,7 @@ int WP_SaberInitBladeData( gentity_t *ent )
 		{
 			if ( !ent->client->ps.saberAnimLevel )
 			{
-				if ( ent->client->NPC_class == CLASS_DESANN )
+				/**if ( ent->client->NPC_class == CLASS_DESANN )
 				{
 					ent->client->ps.saberAnimLevel = SS_DESANN;
 				}
@@ -913,8 +913,8 @@ int WP_SaberInitBladeData( gentity_t *ent )
 				else if ( ent->NPC && ent->client->playerTeam == TEAM_ENEMY && ent->NPC->rank == RANK_LT )
 				{//boss always starts with strong attacks
 					ent->client->ps.saberAnimLevel = SS_STRONG;
-				}
-				else if ( ent->client->NPC_class == CLASS_PLAYER )
+				}**/
+				if ( ent->client->NPC_class == CLASS_PLAYER )
 				{
 					ent->client->ps.saberAnimLevel = g_entities[0].client->ps.saberAnimLevel;
 				}
